0d49f97688e804c82c85e5ad86a5a0cc2c85e3fb
ladeleba <70828446+ladeleba@users.noreply.github.com>
2020-09-27 10:38:22 -0400

    assgnm

---
+++ javaproject234/src/Operators/MainOperator2.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class MainOperator2 {
+
+	public static void main(String[] args) {
+		int num = 11;
+		int number=num%2;
+		if (number == 0) 
+		{
+			System.out.println("even");
+		}
+		else { System.out.println("not even");			
+		}
+	}
+
+}
---
+++ javaproject234/src/Operators/MainOperators.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class MainOperators {
+
+	public static void main(String[] args) {
+		int a = 25;
+		int b = 36;
+		int c = 19;
+		 if (a>b & a>c)
+		  {System.out.println("a is greater     "+a); }
+		  else if(b>a & b>c)
+		 {System.out.println("b is greater    "+b);}
+		 else if (c>b & c>a)
+		 { System.out.println("c is greater     "+c);}
+		 
+	}
+}
